04cb23ad38ca8fd773b379d7973148b5e4f3ade9
nanako0603 <nnky0603@gmail.com>
2020-06-12 14:39:17 +0900

    Project's Initial Commit

---
+++ www/sketch.js
@@ -0,0 +1,15 @@
+function setup()
+{
+  createCanvas(windowWidth,windowHeight);
+
+ // noCursor();
+}
+function draw()
+ {
+    background(204);
+
+    text("骨",mouseX, mouseY,);
+
+  translate(50,50);  
+  text("犬",mouseX, mouseY,);
+}
