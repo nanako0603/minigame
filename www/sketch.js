c79193abbbf34d58b374fd18dd094a5800297868
nanako0603 <nnky0603@gmail.com>
2020-06-19 15:13:20 +0900

    犬が骨にあたって反応できるようになった（教えてもらった）背景作った

--- www/sketch.js
+++ www/sketch.js
@@ -1,15 +1,37 @@
+var st=20;
+var dogx=300;
+var dogy=300;
+
+
+function preload()
+{
+  img=loadImage('dog.png');
+  img2=loadImage('dogs.png');
+  img3=loadImage('hone.png');
+  img4=loadImage('haikei5.png');
+}
+
+
 function setup()
 {
   createCanvas(windowWidth,windowHeight);
-
- // noCursor();
+  imageMode(CENTER);
 }
+
+
 function draw()
  {
-    background(204);
+  image(img4,500,650,1500,1500);
 
-    text("骨",mouseX, mouseY,);
+  image(img3,mouseX, mouseY,350,350);
 
-  translate(50,50);  
-  text("犬",mouseX, mouseY,);
+  translate(1,1);
+  if(abs(dogx-mouseX) < 200 && abs(dogy-mouseY) < 200)
+  {
+    image(img2,dogx+=(mouseX-dogx)/st,dogy+=(mouseY-dogy)/st,450,450);
+  }
+  else
+  {
+    image(img,dogx+=(mouseX-dogx)/st,dogy+=(mouseY-dogy)/st,450,450);
+  }
 }
